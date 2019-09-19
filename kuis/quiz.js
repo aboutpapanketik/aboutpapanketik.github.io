418e9c63a89e079900d2266871f73c055d7920d3
aboutpapanketik <setiawanzanas@gmail.com>
2019-09-19 21:45:41 +0700

    initial commit

---
+++ kuis/quiz.js
@@ -0,0 +1,630 @@
+var questions = _.shuffle([{
+        question: "Pada tahun berapa mesin tik dipasarkan secara massal?",
+        choices: _.shuffle([{
+                choice: "1874",
+                answer: 1
+            },
+            {
+                choice: "1875",
+                answer: 0
+            },
+            {
+                choice: "1876",
+                answer: 0
+            },
+            {
+                choice: "1877",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Pada tahun berapa teletype machine ditemukan? ",
+        choices: _.shuffle([{
+                choice: "1930",
+                answer: 1
+            },
+            {
+                choice: "1931",
+                answer: 0
+            },
+            {
+                choice: "1932",
+                answer: 0
+            },
+            {
+                choice: "1933",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Jumlah tombol keseluruhan pada keyboard adalah?",
+        choices: _.shuffle([{
+                choice: "104",
+                answer: 1
+            },
+            {
+                choice: "54",
+                answer: 0
+            },
+            {
+                choice: "55",
+                answer: 0
+            },
+            {
+                choice: "56",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Siapa pelopor dari penemu susunan tombol keyboard QWERTY?",
+        choices: _.shuffle([{
+                choice: "Christopher Latham Scholes",
+                answer: 0
+            },
+            {
+                choice: "Christopher Latham Sholes",
+                answer: 1
+            },
+            {
+                choice: "Bill Gates",
+                answer: 0
+            },
+            {
+                choice: "Douglas Engelberd",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Key yang didalamnya terdapat angka-angka dan arrow key adalah?",
+        choices: _.shuffle([{
+                choice: "Typewriter Key",
+                answer: 0
+            },
+            {
+                choice: "Numeric Key",
+                answer: 1
+            },
+            {
+                choice: "Function Key",
+                answer: 0
+            },
+            {
+                choice: "Alphanumeric Key",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Jenis-jenis keyboard dibagi menjadi dua, yaitu?",
+        choices: _.shuffle([{
+                choice: "Secara fisik dan tombol",
+                answer: 1
+            },
+            {
+                choice: "Secara tombol dan pembuatan",
+                answer: 0
+            },
+            {
+                choice: "Secara bentuk dan pembuatan",
+                answer: 0
+            },
+            {
+                choice: "Secara fisik dan pembuatan",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Yang termasuk jenis-jenis keyboard komputer secara bentuk dan tombol, kecuali? ",
+        choices: _.shuffle([{
+                choice: "Wireless",
+                answer: 1
+            },
+            {
+                choice: "Qwerty",
+                answer: 0
+            },
+            {
+                choice: "Dvorak",
+                answer: 0
+            },
+            {
+                choice: "Alphabetic",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Jenis keyboard computer secara fisik yang jarag digunakan untuk saat ini adalah?",
+        choices: _.shuffle([{
+                choice: "Keyboard Serial",
+                answer: 1
+            },
+            {
+                choice: "Keyboard USB",
+                answer: 0
+            },
+            {
+                choice: "Keyboard Wireless",
+                answer: 0
+            },
+            {
+                choice: "Keyboard PS/2",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Key yang terdiri dari back space, delete, caps lock, esc, enter, home, insert, page up, page down, dsb adalah? ",
+        choices: _.shuffle([{
+                choice: "Typewriter key",
+                answer: 1
+            },
+            {
+                choice: "Numeric key",
+                answer: 0
+            },
+            {
+                choice: "Alphanumeric key",
+                answer: 0
+            },
+            {
+                choice: "Function key",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Jenis tombol keyboard yang diciptakan pada tahun 1932 adalah? ",
+        choices: _.shuffle([{
+                choice: "Dvorak",
+                answer: 1
+            },
+            {
+                choice: "Qwerty",
+                answer: 0
+            },
+            {
+                choice: "Alphabetic",
+                answer: 0
+            },
+            {
+                choice: "Alphanumeric",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Tombol pada keyboard yang berfungsi untuk membatalkan perintah adalah? ",
+        choices: _.shuffle([{
+                choice: "Backspace",
+                answer: 1
+            },
+            {
+                choice: "Delete",
+                answer: 0
+            },
+            {
+                choice: "Enter",
+                answer: 0
+            },
+            {
+                choice: "Esc",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Yang dimaksud dengan keyboard adalah?",
+        choices: _.shuffle([{
+                choice: "Papan tombol ketik",
+                answer: 1
+            },
+            {
+                choice: "Alat untuk mencetak",
+                answer: 0
+            },
+            {
+                choice: "Program Grafik",
+                answer: 0
+            },
+            {
+                choice: "Alat untuk menghasilkan ketikan",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Keyboard USB biasanya digunakan pada komputer jenis? ",
+        choices: _.shuffle([{
+                choice: "USB",
+                answer: 1
+            },
+            {
+                choice: "ATX",
+                answer: 0
+            },
+            {
+                choice: "AT/2",
+                answer: 0
+            },
+            {
+                choice: "ATT",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Fungsi tombol yang memberikan perintah menghidupkan dan mematikan fungsi tombol numerik adalah?",
+        choices: _.shuffle([{
+                choice: "Numeric key",
+                answer: 1
+            },
+            {
+                choice: "Alphanumeric key",
+                answer: 0
+            },
+            {
+                choice: "Function key",
+                answer: 0
+            },
+            {
+                choice: "Page up",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Keyboard pertama kali diperkenalkan dan diciptakan oleh Christopher Latham Sholes pada tahun 1868 sebanyak?",
+        choices: _.shuffle([{
+                choice: "104 tombol",
+                answer: 1
+            },
+            {
+                choice: "103 tombol",
+                answer: 0
+            },
+            {
+                choice: "105 tombol",
+                answer: 0
+            },
+            {
+                choice: "106 tombol",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Sebuah perangkat keras (hardware) pada komputer yang berfungsi sebagai alat untuk input data yang berupa huruf, angka dan simbol yaitu ?",
+        choices: _.shuffle([{
+                choice: "Keyboard",
+                answer: 1
+            },
+            {
+                choice: "Mouse",
+                answer: 0
+            },
+            {
+                choice: "Printer",
+                answer: 0
+            },
+            {
+                choice: "Scanner",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Mana hal berikut yang bukan merupakan fungsi dari keyboard? ",
+        choices: _.shuffle([{
+                choice: "Menekan tombol enter untuk menghapus hasil ketikan",
+                answer: 1
+            },
+            {
+                choice: "Menekan tombol panah pada keyboard untuk menggerakkan kursor",
+                answer: 0
+            },
+            {
+                choice: "Menekan tombol shift untuk menempilkan huruf besar atau kecil",
+                answer: 0
+            },
+            {
+                choice: "Menekan tombol esc untuk membatalkan perintah",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Jumlah tombol numerik pada keyboard adalah?",
+        choices: _.shuffle([{
+                choice: "10",
+                answer: 1
+            },
+            {
+                choice: "9",
+                answer: 0
+            },
+            {
+                choice: "11",
+                answer: 0
+            },
+            {
+                choice: "17",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Di bawah ini adalah hal-hal yang perlu diperhatikan dalam mengetik mengunakan 10 jari, kecuali? ",
+        choices: _.shuffle([{
+                choice: "Emosi",
+                answer: 1
+            },
+            {
+                choice: "Kuasai bahasa inggris",
+                answer: 0
+            },
+            {
+                choice: "Konsentrasi",
+                answer: 0
+            },
+            {
+                choice: "Pelajari setiap hari",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Untuk mengaktifkan submenu print, tombol keyboard yang kita gunakan adalah? ",
+        choices: _.shuffle([{
+                choice: "Ctrl+P ",
+                answer: 1
+            },
+            {
+                choice: "Alt+ P",
+                answer: 0
+            },
+            {
+                choice: "Ctrl+Q ",
+                answer: 0
+            },
+            {
+                choice: "Alt+V",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Pencipta keyboard DVORAK adalah?",
+        choices: _.shuffle([{
+                choice: "August Dvorak",
+                answer: 1
+            },
+            {
+                choice: "Bill Gates",
+                answer: 0
+            },
+            {
+                choice: "Charles Babbage",
+                answer: 0
+            },
+            {
+                choice: "Christoper Latham",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Tombol keyboard yang memiliki fungsi hampir sama dengan Caps Lock adalah?",
+        choices: _.shuffle([{
+                choice: "Esc",
+                answer: 1
+            },
+            {
+                choice: "Tab",
+                answer: 0
+            },
+            {
+                choice: "Alt",
+                answer: 0
+            },
+            {
+                choice: "Shift",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Untuk menghapus huruf yang ada disebelah kanan kursor menggunakan tombol? ",
+        choices: _.shuffle([{
+                choice: "Delete",
+                answer: 1
+            },
+            {
+                choice: "CTRL",
+                answer: 0
+            },
+            {
+                choice: "Backspace",
+                answer: 0
+            },
+            {
+                choice: "Esc",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Tombol Ctrl+B digunakan untuk?",
+        choices: _.shuffle([{
+                choice: "Membuat huruf menjadi tebal",
+                answer: 1
+            },
+            {
+                choice: "Membuat garis tepi pada halaman",
+                answer: 0
+            },
+            {
+                choice: "Membuat perataan huruf menjadi rata tengah",
+                answer: 0
+            },
+            {
+                choice: "Membatalkan perintah yang dimasukkan",
+                answer: 0
+            }
+        ])
+    },
+    {
+        question: "Up, Down, Right, Left, Page Up, Page Down, Home, dan End termasuk dalam kelompok tombol? ",
+        choices: _.shuffle([{
+                choice: "Navigasi",
+                answer: 1
+            },
+            {
+                choice: "Edit",
+                answer: 0
+            },
+            {
+                choice: "Modifikasi",
+                answer: 0
+            },
+            {
+                choice: "Ketik",
+                answer: 0
+            }
+        ])
+    },                      
+]);
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
+var numQuestions = questions.length - 20;
+
+var userAnswer = [];
+
+var currentQuestionText = [];
+
+var teste = [];
+
+$(document).ready(function () {
+
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+
+    $(this).find(".nextButton").on("click", function () {
+        if (!quizOver) {
+
+            value = $("input[type='radio']:checked").val();
+
+            if (value == undefined) {
+                $(document).find(".quizMessage").text("Pilih Terlebih Dahulu");
+                $(document).find(".quizMessage").show();
+            } else {
+
+                userAnswer.push(questions[currentQuestion].choices[value].choice);
+
+                currentQuestionText.push(questions[currentQuestion].question);
+
+                teste.push(questions[currentQuestion].choices[value].answer);
+
+
+                $(document).find(".quizMessage").hide();
+
+
+                if (questions[currentQuestion].choices[value].answer === 1) {
+                    correctAnswers++;
+                }
+
+                currentQuestion++;
+
+                if (currentQuestion < numQuestions) {
+                    displayCurrentQuestion();
+                } else {
+                    displayScore();
+                    $(document).find(".nextButton").text("Kembali Ke Home");
+                    quizOver = true;
+                }
+            }
+        } else {
+            startScreen();
+        }
+    });
+});
+
+
+
+function displayCurrentQuestion() {
+
+    console.log("In display current Question");
+
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+
+
+    $(questionClass).text((currentQuestion + 1) + '. ' + question);
+
+
+    $(choiceList).find("li").remove();
+
+    var choice;
+    var answer;
+    for (i = 0; i < numChoices; i++) {
+
+        choice = questions[currentQuestion].choices[i].choice;
+        $('<li><input type="radio" id="item' + i + '" value=' + i + ' name="dynradio" /><label for="item' + i + '">' + choice + '</label></li>').appendTo(choiceList);
+    }
+}
+
+function startScreen() {
+    quizOver = false;
+    $(document).find(".nextButton");
+    resetQuiz();
+    hideScore();
+    window.location.href = "index.html#kuis";
+}
+
+
+function resetQuiz() {
+    location.reload();
+    currentQuestion = 0;
+    correctAnswers = 0;
+    userAnswer = [];
+    currentQuestionText = [];
+    hideScore();
+}
+
+
+function displayScore() {
+
+
+    console.log("Teste: " + teste);
+
+    var feedback = $(document).find(".quizContainer > .result");
+
+    $(document).find(".question").hide();
+    $(document).find(".choiceList").hide();
+    $(document).find(".quizContainer > .result").text("Jumlah Skor Anda " + correctAnswers + " / " + numQuestions);
+
+
+
+    if (correctAnswers >= 3) {
+        $('').appendTo(feedback);
+    } else {
+        $('').appendTo(feedback);
+    }
+
+
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() {
+    $(document).find(".result").hide();
+}
